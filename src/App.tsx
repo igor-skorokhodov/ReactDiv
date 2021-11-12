8ec1fb902ea21144cba5ef26578e5863b7350372
Plat0 <plat0@MacBook-Pro-Plat0.local>
2021-11-12 17:45:04 +0300

    new

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewDiv from '../src/components/NewDiv';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NewDiv />
+    </div>
+  );
+}
+
+export default App;
