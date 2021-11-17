ce6eb1d5a20e91a778ff88b3b61910a079318345
calabanprivate <alaeff@gmail.com>
2021-11-17 18:12:25 +0300

    поправки небольшие

--- src/components/NewDiv.tsx
+++ src/components/NewDiv.tsx
@@ -2,90 +2,81 @@ import React from "react";
 import "../components/NewDiv.css";
 
 interface MyState {
-  beginX?: number;
-  beginY?: number;
-  endX?: number;
-  endY?: number;
-  isClicked?: boolean;
-  width?: number;
-  height?: number;
+    beginX?: number;
+    beginY?: number;
+    endX?: number;
+    endY?: number;
+    isClicked?: boolean;
+    width?: number;
+    height?: number;
 }
 
 class NewDiv extends React.Component<MyState> {
-  constructor(props: any) {
-    super(props);
-    this.state = { isClicked: false};
-  }
+    constructor(props: any) {
+        super(props);
+        this.state = { isClicked: false };
+    }
 
-  mouseDown(e: any) {
-    this.setState({
-      beginX: e.pageX,
-      beginY: e.pageY,
-      width: 0,
-      height: 0,
-      isClicked: !(this.state as any).isClicked
-    });
-  }
+    mouseDown(e: any) {
+        this.setState({
+            beginX: e.pageX,
+            beginY: e.pageY,
+            width: 0,
+            height: 0,
+            isClicked: true
+        });
+    }
 
-  handleMouseMove(e: any) {
-    this.setState({
-      endX: e.pageX,
-      endY: e.pageY,
-      width: e.pageX - (this.state as any).beginX,
-      height: e.pageY - (this.state as any).beginY,
-    });
-    console.log("444")
-  }
+    handleMouseMove(e: any) {
+        this.setState({
+            endX: e.pageX,
+            endY: e.pageY,
+            width: e.pageX - (this.state as any).beginX,
+            height: e.pageY - (this.state as any).beginY,
+        });
+    }
 
-  mouseUp(e: any) {
-    this.setIsClicked();
-    console.log((this.state as any).isClicked);
-  }
+    mouseUp(e: any) {
+        this.setIsClickedFalse();
+    }
 
-  setIsClicked() {
-    this.setState({ isClicked: !(this.state as any).isClicked });
-  }
+    setIsClicked() {
+        this.setState({ isClicked: true });
+    }
 
-  setIsClickedFalse() {
-    this.setState({ isClicked: false });
-    console.log((this.state as any).isClicked);
-  }
+    setIsClickedFalse() {
+        this.setState({ isClicked: false });
+    }
 
-  render() {
-    return (
-      <div className="NewDiv">
-        <header className="App-header">
-          <p>Div для рисования divов</p>
-        </header>
-        <div
-          className="field"
-          onMouseDown={this.mouseDown.bind(this)}
-          onMouseUp={this.mouseUp.bind(this)}
-          onMouseOut={this.setIsClickedFalse.bind(this)}
-          onMouseMove={(this.state as any).isClicked ? (e: any) => {
-            console.log("3333")
-            this.handleMouseMove(e);
-          } : undefined}
-        >
-          {(this.state as any).isClicked ? (
-            <div
-              className="div-selected"
-              style={{
-                top: (this.state as any).beginY,
-                left: (this.state as any).beginX,
-                width: (this.state as any).width,
-                height: (this.state as any).height,
-                position: "absolute",
-                border: "1px solid black",
-              }}
-            ></div>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="NewDiv">
+                <header className="App-header">
+                    <p>Div для рисования divов</p>
+                </header>
+                <div
+                    className="field"
+                    onMouseDown={this.mouseDown.bind(this)}
+                    onMouseUp={this.mouseUp.bind(this)}
+                    // onMouseOut={this.setIsClickedFalse.bind(this)}
+                    onMouseMove={(this.state as any).isClicked ? (e: any) => {
+                        this.handleMouseMove(e);
+                    } : undefined}>
+                    {(this.state as any).isClicked ? (
+                        <div
+                            className="div-selected"
+                            style={{
+                                top: (this.state as any).beginY,
+                                left: (this.state as any).beginX,
+                                width: (this.state as any).width,
+                                height: (this.state as any).height,
+                            }}
+                        ></div>
+                    ) : null}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default NewDiv;
